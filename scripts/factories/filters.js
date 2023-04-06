8189cbc8503c3fae40d0bc13e866094df9c0b4ac
Jezequel_Maxence <maxence.jezequel@hotmail.com>
2023-04-06 22:57:45 +0200

    Menu filter integration DOM

--- scripts/factories/filters.js
+++ scripts/factories/filters.js
@@ -1,5 +1,4 @@
-
-export class Recipe{
+export class FilterInterface{
 
     constructor(data){
         this._id = data.id
@@ -10,42 +9,6 @@ export class Recipe{
         this._description = data.description
         this._appliance = data.appliance
         this._ustensils = data.ustensils
-    }
-
-    /*Création de la carte de la recette*/
-    createhtmlRecipeCard(){
-        return`
-        <div class="recipes__cards">
-        <div class="imgcards__containers"></div>
-        <div class="recipescards__containers">
-        <h2>${this._name}</h2>
-        <p><i class="fa-regular fa-clock"></i> ${this._time} min</p>
-        <p>${this.createIngrediants()}</p>
-        <p>${this._description}</p>
-        </div>
-        </div>`
-    }
-
-    /*Création de la liste des ingrediants pour l'affichache */
-    createIngrediants(){
-        let html = ""
-        console.log(this._ingredients)
-        this._ingredients.forEach(element => {
-            console.log(element)
-            
-            let ingredient = ""
-            let quantity = ""
-            let unit = ""
-            if(element.unit){
-                unit = element.unit
-            }
-            if(element.quantity){
-                quantity = element.quantity
-            }
-            html += `${element.ingredient} : ${quantity}${unit}`
-        });
-        console.log(html)
-        return html
     }
 
     filterIngredients(){
@@ -84,5 +47,4 @@ export class Recipe{
 
         </div>`
     }
-
 }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,15 +1,23 @@
 import { recipes } from "../../data/recipes.js"
 import { Recipe } from "../factories/recipes.js"
 import { search } from "../utils/search_input.js"
+import { FilterInterface } from "../factories/filters.js"
 
 /*Afficher les Données*/
 async function displayData(data) {
     const recipesSectionCards = document.querySelector(".recipes__cards--all");
+    const filtersIngredientsSection = document.querySelector(".filters__ingredients")
+    const filtersAppareilsSection = document.querySelector(".filters__appliances")
+    const filtersUstensilsSection = document.querySelector(".filters__ustensils")
     data.forEach((recipes) => {
         const recipesModel = new Recipe(recipes);
         const recipesCardDOM = recipesModel.createhtmlRecipeCard();
         recipesSectionCards.innerHTML=recipesSectionCards.innerHTML+(recipesCardDOM);
+
     });
+    const filterModel = new FilterInterface(recipes);
+    const filterDOM = filterModel.filterIngredients();
+    filtersIngredientsSection.innerHTML=filtersIngredientsSection.innerHTML+(filterDOM);
 }
 
 async function init() {
